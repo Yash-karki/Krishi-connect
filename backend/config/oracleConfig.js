916119eb09c61a83526b0bdda3b82bcece053a40
Yash-karki <yashkarki61@gmail.com>
2025-08-25 20:12:45 +0530

    chng

--- backend/config/oracleConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  user: "system",         // Oracle DB username
-  password: "oracle123",  // Oracle DB password
-  connectString: "localhost/XE" // Oracle XE or DB service name
-};
