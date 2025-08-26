64fc3e48b59a46ae1e425054cb847f75b87cec29
Pankaj Aghariya <aghariyapankaj001@gmail.com>
2025-08-26 07:32:35 +0530

    new features

--- backend/routes/dashboardroutes.js
+++ backend/routes/dashboardroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getDashboard } = require('../controllers/dashboardController');
+const { getDashboard } = require('../controllers/dashboardcontrollers');
 const { authRequired } = require('../middlewares/authmiddleware');
 
 router.get('/', authRequired, getDashboard);
